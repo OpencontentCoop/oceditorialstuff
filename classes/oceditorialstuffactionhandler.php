a953d6d9b9faaa8723a9777c5c21a8e27c0c512e
Luca Realdi <luca.realdi@opencontent.it>
2015-06-22 16:33:58 +0200

    AddLocation and RemoveLocation out of the box

--- classes/oceditorialstuffactionhandler.php
+++ classes/oceditorialstuffactionhandler.php
@@ -3,43 +3,127 @@
 class OCEditorialStuffActionHandler
 {
 
+    private static $_instances = array();
+
     /**
      * @var OCEditorialStuffPostFactoryInterface
      */
     protected $factory;
 
+    /**
+     * @var array
+     */
+    protected $factoryActionConfiguration = array();
+
+    protected $availableActions = array();
+
     protected function __construct( OCEditorialStuffPostFactoryInterface $factory )
     {
+        $editorialIni = eZINI::instance( 'editorialstuff.ini' );
+        $availableActions = $editorialIni->variable( 'AvailableActions', 'Actions' );
+        foreach( $availableActions as $actionName )
+        {
+            if ( $editorialIni->hasGroup( $actionName ) )
+            {
+                $className = $editorialIni->variable( $actionName, 'ClassName' );
+                $methodName = $editorialIni->variable( $actionName, 'MethodName' );
+                if ( method_exists( $className, $methodName ) )
+                {
+                    $this->availableActions[$actionName] = array( $className, $methodName );
+                }
+                else
+                {
+                    eZDebug::writeError( "{$className}::{$methodName} not callable", __METHOD__ );
+                }
+            }
+        }
         $this->factory = $factory;
         $factoryConfiguration = $this->factory->getConfiguration();
-        
+        $statesConfigurations = $factoryConfiguration['States'];
+        $actionsConfiguration = $factoryConfiguration['Actions'];
+        foreach( $actionsConfiguration as $changeStates => $actionAndSettings )
+        {
+            $configurationItem = array();
+            $changeStatesParts = explode( '-', $changeStates );
+            $actionParameters = explode( ';', $actionAndSettings );
+            $actionName = array_shift( $actionParameters );
+            if ( count( $changeStatesParts ) == 2
+                 && isset( $statesConfigurations[$changeStatesParts[0]] )
+                 && isset( $statesConfigurations[$changeStatesParts[1]] )
+                 && isset( $this->availableActions[$actionName] ) )
+            {
+                $configurationItem['before_state'] = $changeStatesParts[0];
+                $configurationItem['after_state'] = $changeStatesParts[1];
+                $configurationItem['call_function'] = $this->availableActions[$actionName];
+                $configurationItem['call_function_parameters'] = $actionParameters;
+                $this->factoryActionConfiguration[$changeStates] = $configurationItem;
+            }
+        }
     }
 
     public static function instance( OCEditorialStuffPostFactoryInterface $factory )
     {
-        return new OCEditorialStuffActionHandler( $factory );
+        $factoryClassName = get_class( $factory );
+        if ( !isset( self::$_instances[$factoryClassName] ) )
+        {
+            self::$_instances[$factoryClassName] = new OCEditorialStuffActionHandler(
+                $factory
+            );
+        }
+        return self::$_instances[$factoryClassName];
     }
 
     public function handleChangeState( OCEditorialStuffPost $post, eZContentObjectState $beforeState, eZContentObjectState $afterState )
     {
-
+        $changeStateString = $beforeState->attribute( 'identifier' ) . '-' . $afterState->attribute( 'identifier' );
+        if ( isset( $this->factoryActionConfiguration[$changeStateString] ) )
+        {
+            $parameters = array_merge( array( $post ), array( $this->factoryActionConfiguration[$changeStateString]['call_function_parameters'] ) );
+            call_user_func_array( $this->factoryActionConfiguration[$changeStateString]['call_function'], $parameters );
+        }
     }
 
     // out of the box actions
     public static function addLocation( OCEditorialStuffPost $post,
-                                        $addLocationId )
+                                        $addLocationIds )
     {
-
+        $object = $post->getObject();
+        if ( $object instanceof eZContentObject )
+        {
+            eZContentOperationCollection::addAssignment(
+                $object->attribute( 'main_node_id' ),
+                $object->attribute( 'id' ),
+                $addLocationIds
+            );
+        }
+        else
+        {
+            eZDebug::writeError( "Object not found", __METHOD__ );
+        }
     }
 
     public static function removeLocation( OCEditorialStuffPost $post,
-                                           $removeLocationId )
+                                           $removeLocationIds )
     {
-
-    }
-
-    public static function move( OCEditorialStuffPost $post, $locationId )
-    {
-
+        /** @var eZContentObjectTreeNode[] $nodes */
+        $nodes = $post->getObject()->attribute( 'assigned_nodes' );
+        $removeNodeIdList = array();
+        if ( count( $nodes ) > 1 )
+        {
+            foreach ( $nodes as $node )
+            {
+                foreach( $removeLocationIds as $removeLocationId )
+                {
+                    if ( $node->attribute( 'parent_node_id' ) == $removeLocationId )
+                    {
+                        $removeNodeIdList[] = $node->attribute( 'node_id' );
+                    }
+                }
+            }
+        }
+        if ( !empty( $removeNodeIdList ) )
+        {
+            eZContentOperationCollection::removeNodes( $removeNodeIdList );
+        }
     }
 }
