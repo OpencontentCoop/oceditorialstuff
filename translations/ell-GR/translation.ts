da81ca15630b873e7764e9dac0d1aebeda79265b
Luca Realdi <lr@opencontent.it>
2019-12-16 15:31:35 +0100

    Aggiunge il file di traduzione delle stringhe in greco

---
+++ translations/ell-GR/translation.ts
@@ -0,0 +1,253 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+  <context>
+    <name>editorialstuff/dashboard</name>
+    <message>
+      <source>Search</source>
+      <translation>Ψάξιμο</translation>
+    </message>
+    <message>
+      <source>All</source>
+      <translation>Ολα</translation>
+    </message>
+    <message>
+      <source>Period</source>
+      <translation>Περίοδος</translation>
+    </message>
+    <message>
+      <source>Author</source>
+      <translation>Συγγραφέας</translation>
+    </message>
+    <message>
+      <source>Date</source>
+      <translation>Ημερομηνία</translation>
+    </message>
+    <message>
+      <source>State</source>
+      <translation>κατάσταση</translation>
+    </message>
+    <message>
+      <source>Title</source>
+      <translation>Τίτλος</translation>
+    </message>
+    <message>
+      <source>Detail</source>
+      <translation>Λεπτομέρεια</translation>
+    </message>
+    <message>
+      <source>Content</source>
+      <translation>Περιεχόμενο</translation>
+    </message>
+    <message>
+      <source>Media</source>
+      <translation>Μεσο ΜΑΖΙΚΗΣ ΕΝΗΜΕΡΩΣΗΣ</translation>
+    </message>
+    <message>
+      <source>Mail</source>
+      <translation>Ταχυδρομείο</translation>
+    </message>
+    <message>
+      <source>Social Network</source>
+      <translation>Κοινωνικό δίκτυο</translation>
+    </message>
+    <message>
+      <source>History</source>
+      <translation>Ιστορία</translation>
+    </message>
+    <message>
+      <source>Edit</source>
+      <translation>Επεξεργασία</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Προεπισκόπηση</translation>
+    </message>
+    <message>
+      <source>Publication date</source>
+      <translation>Ημερομηνία έκδοσης</translation>
+    </message>
+    <message>
+      <source>Collocations</source>
+      <translation>Συνεγκαταστάσεων</translation>
+    </message>
+    <message>
+      <source>Action</source>
+      <translation>Δράση</translation>
+    </message>
+    <message>
+      <source>Receivers</source>
+      <translation>δέκτες</translation>
+    </message>
+    <message>
+      <source>Search user</source>
+      <translation>Αναζήτηση χρήστη</translation>
+    </message>
+    <message>
+      <source>Enter an email address per line</source>
+      <translation>Πληκτρολογήστε μια διεύθυνση ηλεκτρονικού ταχυδρομείου ανά γραμμή</translation>
+    </message>
+    <message>
+      <source>Message</source>
+      <translation>Μήνυμα</translation>
+    </message>
+    <message>
+      <source>Optional: appears at the beginning of the message.</source>
+      <translation>Προαιρετικό: Εμφανίζεται στην αρχή του μηνύματος.</translation>
+    </message>
+    <message>
+      <source>Include approval features</source>
+      <translation>Περιλαμβάνουν χαρακτηριστικά έγκριση</translation>
+    </message>
+    <message>
+      <source>Send email</source>
+      <translation>Να στείλετε e-mail</translation>
+    </message>
+    <message>
+      <source>Sender</source>
+      <translation>Αποστολέας</translation>
+    </message>
+    <message>
+      <source>Errors</source>
+      <translation>Λάθη</translation>
+    </message>
+    <message>
+      <source>Last day</source>
+      <translation>Τελευταία μέρα</translation>
+    </message>
+    <message>
+      <source>Last 7 days</source>
+      <translation>Τελευταίες 7 ημέρες</translation>
+    </message>
+    <message>
+      <source>Last 30 days</source>
+      <translation>Τελευταίες 30 ημέρες</translation>
+    </message>
+    <message>
+      <source>Last two months</source>
+      <translation>Τελευταίων δύο μηνών</translation>
+    </message>
+    <message>
+      <source>Last editor</source>
+      <translation>τελευταία editor</translation>
+    </message>
+    <message>
+      <source>Locations</source>
+      <translation>τοποθεσίες</translation>
+    </message>
+    <message>
+      <source>Current state is</source>
+      <translation>Η τρέχουσα κατάσταση είναι</translation>
+    </message>
+    <message>
+      <source>Change state to</source>
+      <translation>Αλλαγή κράτος να</translation>
+    </message>
+    <message>
+      <source>View on website</source>
+      <translation>Δείτε στην ιστοσελίδα</translation>
+    </message>
+    <message>
+      <source>Close</source>
+      <translation>Κοντά</translation>
+    </message>
+    <message>
+      <source>Add new</source>
+      <translation>Προσθεσε νεο</translation>
+    </message>
+    <message>
+      <source>From library</source>
+      <translation>από τη βιβλιοθήκη</translation>
+    </message>
+  </context>
+  <context>
+    <name>agenda/dashboard</name>
+    <message>
+      <source>Add new</source>
+      <translation>Προσθεσε νεο</translation>
+    </message>
+    <message>
+      <source>From library</source>
+      <translation>από τη βιβλιοθήκη</translation>
+    </message>
+  </context>
+  <context>
+    <name>editorialstuff/mail</name>
+    <message>
+      <source>Do you want to change the content state?</source>
+      <translation>Θέλετε να αλλάξετε την κατάσταση περιεχόμενο;</translation>
+    </message>
+    <message>
+      <source>The current state is</source>
+      <translation>Η σημερινή κατάσταση είναι</translation>
+    </message>
+    <message>
+      <source>Change state to</source>
+      <translation>Αλλαγή κράτος να</translation>
+    </message>
+    <message>
+      <source>Dashboard</source>
+      <translation>Ταμπλό</translation>
+    </message>
+    <message>
+      <source>Author</source>
+      <translation>Συγγραφέας</translation>
+    </message>
+    <message>
+      <source>Publication date</source>
+      <translation>Ημερομηνία έκδοσης</translation>
+    </message>
+    <message>
+      <source>Add audio</source>
+      <translation>Προσθήκη ήχου</translation>
+    </message>
+    <message>
+      <source>Add image</source>
+      <translation>Προσθέστε εικόνα</translation>
+    </message>
+    <message>
+      <source>Add video</source>
+      <translation>Προσθήκη βίντεο</translation>
+    </message>
+    <message>
+      <source>New version</source>
+      <translation>Νέα έκδοση</translation>
+    </message>
+    <message>
+      <source>Set default image audio</source>
+      <translation>Ορισμός προεπιλεγμένης ήχου εικόνας</translation>
+    </message>
+    <message>
+      <source>Remove audio</source>
+      <translation>Κατάργηση ήχου</translation>
+    </message>
+    <message>
+      <source>Remove image</source>
+      <translation>αφαίρεση της εικόνας</translation>
+    </message>
+    <message>
+      <source>Remove video</source>
+      <translation>Κατάργηση βίντεο</translation>
+    </message>
+    <message>
+      <source>Change state from</source>
+      <translation>Αλλαγή κράτος από</translation>
+    </message>
+    <message>
+      <source>to</source>
+      <translation>προς το</translation>
+    </message>
+    <message>
+      <source>Notification about</source>
+      <translation>Γνωστοποίηση</translation>
+    </message>
+    <message>
+      <source>The content</source>
+      <translation>Το περιεχόμενο</translation>
+    </message>
+    <message>
+      <source>has been changed status</source>
+      <translation>έχει αλλάξει το καθεστώς</translation>
+    </message>
+  </context>
+</TS>
