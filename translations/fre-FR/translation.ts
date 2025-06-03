c35c087072fbcc6a46d6f86f8aaea6c989488476
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:39:34 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>editorialstuff/dashboard</name>
+    <message>
+      <source>Search</source>
+      <translation>Rechercher</translation>
+    </message>
+    <message>
+      <source>All</source>
+      <translation>Tous</translation>
+    </message>
+    <message>
+      <source>Period</source>
+      <translation>Période</translation>
+    </message>
+    <message>
+      <source>Author</source>
+      <translation>Auteur</translation>
+    </message>
+    <message>
+      <source>Date</source>
+      <translation>Date</translation>
+    </message>
+    <message>
+      <source>State</source>
+      <translation>État</translation>
+    </message>
+    <message>
+      <source>Title</source>
+      <translation>Titre</translation>
+    </message>
+    <message>
+      <source>Detail</source>
+      <translation>Détail</translation>
+    </message>
+    <message>
+      <source>Content</source>
+      <translation>Contenu</translation>
+    </message>
+    <message>
+      <source>Media</source>
+      <translation>Moyenne</translation>
+    </message>
+    <message>
+      <source>Mail</source>
+      <translation>E-mail</translation>
+    </message>
+    <message>
+      <source>Social Network</source>
+      <translation>Réseaux sociaux</translation>
+    </message>
+    <message>
+      <source>History</source>
+      <translation>Historique</translation>
+    </message>
+    <message>
+      <source>Edit</source>
+      <translation>Modifier</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Aperçu</translation>
+    </message>
+    <message>
+      <source>Publication date</source>
+      <translation>Date de publication</translation>
+    </message>
+    <message>
+      <source>Collocations</source>
+      <translation>Emplacements</translation>
+    </message>
+    <message>
+      <source>Action</source>
+      <translation>Action</translation>
+    </message>
+    <message>
+      <source>Receivers</source>
+      <translation>Destinataires</translation>
+    </message>
+    <message>
+      <source>Search user</source>
+      <translation>Rechercher un utilisateur</translation>
+    </message>
+    <message>
+      <source>Enter an email address per line</source>
+      <translation>Saisir une adresse e-mail par ligne</translation>
+    </message>
+    <message>
+      <source>Message</source>
+      <translation>Message</translation>
+    </message>
+    <message>
+      <source>Optional: appears at the beginning of the message.</source>
+      <translation>Facultatif : apparaît au début du message.</translation>
+    </message>
+    <message>
+      <source>Include approval features</source>
+      <translation>Inclure les fonctions d’approbation</translation>
+    </message>
+    <message>
+      <source>Send email</source>
+      <translation>Envoyer un e-mail</translation>
+    </message>
+    <message>
+      <source>Sender</source>
+      <translation>Expéditeur</translation>
+    </message>
+    <message>
+      <source>Errors</source>
+      <translation>Erreurs</translation>
+    </message>
+    <message>
+      <source>Last day</source>
+      <translation>Dernier jour</translation>
+    </message>
+    <message>
+      <source>Last 7 days</source>
+      <translation>7 derniers jours</translation>
+    </message>
+    <message>
+      <source>Last 30 days</source>
+      <translation>30 derniers jours</translation>
+    </message>
+    <message>
+      <source>Last two months</source>
+      <translation>Deux derniers mois</translation>
+    </message>
+    <message>
+      <source>Last editor</source>
+      <translation>Dernière modification de</translation>
+    </message>
+    <message>
+      <source>Locations</source>
+      <translation>Emplacements</translation>
+    </message>
+    <message>
+      <source>Current state is</source>
+      <translation>L’état actuel est</translation>
+    </message>
+    <message>
+      <source>Change state to</source>
+      <translation>Changer l’état à</translation>
+    </message>
+    <message>
+      <source>View on website</source>
+      <translation>Voir sur le site</translation>
+    </message>
+    <message>
+      <source>Close</source>
+      <translation>Fermer</translation>
+    </message>
+    <message>
+      <source>Add new</source>
+      <translation>Ajouter un nouveau</translation>
+    </message>
+    <message>
+      <source>From library</source>
+      <translation>De la bibliothèque</translation>
+    </message>
+    <message>
+      <source>Current post title:</source>
+      <translation>Titre du contenu :</translation>
+    </message>
+    <message>
+      <source>Current post url:</source>
+      <translation>URL du contenu :</translation>
+    </message>
+    <message>
+      <source>Current post status:</source>
+      <translation>État actuel du contenu :</translation>
+    </message>
+    <message>
+      <source>Dashboard url:</source>
+      <translation>URL du tableau de bord :</translation>
+    </message>
+    <message>
+      <source>Site url:</source>
+      <translation>URL du site</translation>
+    </message>
+    <message>
+      <source>Mail customizations</source>
+      <translation>Personnalisation des messages électroniques</translation>
+    </message>
+    <message>
+      <source>If the field is empty, the default text will be sent</source>
+      <translation>Si la valeur n’est pas remplie, le message par défaut sera envoyé</translation>
+    </message>
+    <message>
+      <source>Placeholders:</source>
+      <translation>Repère  :</translation>
+    </message>
+    <message>
+      <source>Before state</source>
+      <translation>État précédent</translation>
+    </message>
+    <message>
+      <source>After state</source>
+      <translation>État actuel</translation>
+    </message>
+    <message>
+      <source>Subject</source>
+      <translation>Titre de l’e-mail</translation>
+    </message>
+    <message>
+      <source>Body</source>
+      <translation>Corps de l’e-mail</translation>
+    </message>
+  </context>
+  <context>
+    <name>agenda/dashboard</name>
+    <message>
+      <source>Add new</source>
+      <translation>Ajouter un nouveau</translation>
+    </message>
+    <message>
+      <source>From library</source>
+      <translation>De la bibliothèque</translation>
+    </message>
+  </context>
+  <context>
+    <name>editorialstuff/mail</name>
+    <message>
+      <source>Do you want to change the content state?</source>
+      <translation>Voulez-vous changer l’état du contenu ?</translation>
+    </message>
+    <message>
+      <source>The current state is</source>
+      <translation>L’état actuel est</translation>
+    </message>
+    <message>
+      <source>Change state to</source>
+      <translation>Changer l’état à</translation>
+    </message>
+    <message>
+      <source>Dashboard</source>
+      <translation>Tableau de bord</translation>
+    </message>
+    <message>
+      <source>Author</source>
+      <translation>Auteur</translation>
+    </message>
+    <message>
+      <source>Publication date</source>
+      <translation>Date de publication</translation>
+    </message>
+    <message>
+      <source>Add audio</source>
+      <translation>Ajouter un audio</translation>
+    </message>
+    <message>
+      <source>Add image</source>
+      <translation>Ajouter une image</translation>
+    </message>
+    <message>
+      <source>Add video</source>
+      <translation>Ajouter une vidéo</translation>
+    </message>
+    <message>
+      <source>New version</source>
+      <translation>Créer une nouvelle version</translation>
+    </message>
+    <message>
+      <source>Set default image audio</source>
+      <translation>Définir l’image par défaut</translation>
+    </message>
+    <message>
+      <source>Remove audio</source>
+      <translation>Audio supprimé</translation>
+    </message>
+    <message>
+      <source>Remove image</source>
+      <translation>Image supprimée</translation>
+    </message>
+    <message>
+      <source>Remove video</source>
+      <translation>Vidéo supprimée</translation>
+    </message>
+    <message>
+      <source>Change state from</source>
+      <translation>L’état a été changé de</translation>
+    </message>
+    <message>
+      <source>to</source>
+      <translation>à</translation>
+    </message>
+    <message>
+      <source>Notification about</source>
+      <translation>Notification sur</translation>
+    </message>
+    <message>
+      <source>The content</source>
+      <translation>Le contenu</translation>
+    </message>
+    <message>
+      <source>has been changed status</source>
+      <translation>a changé d’état</translation>
+    </message>
+  </context>
+</TS>
